d0afeceac9500eb197d578bfb7ea75b35367dd90
wagner machado <wagnermachado52020@gmail.com>
2026-01-16 22:32:11 -0300

    # meta_2026  versão do sst v 5.7.41v controle de CDG 33.17V    versão em testes
    # meta_2026
    
    versão do sst v 5.7.41v
    controle de CDG 33.17V
    
     versão em testes

--- README.md
+++
@@ -1,6 +0,0 @@
-# meta_2026
-
-versão do sst v 5.7.41v
-controle de CDG 32.16V
- 
- versão em testes
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "metaup-cache-v15";
+const CACHE_NAME = "metaup-cache-v16";
 const FILES_TO_IGNORE = ["manifest.json"];
 
 /* Instalação */
